c021f8e760b8a3d47d17868882dda503909c3b03
Konrad Grzanek <kongra@gmail.com>
2014-10-25 08:43:44 +0200

    Initial version of the project.

---
+++ AI/Search/Tree.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE CPP #-}
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Trustworthy #-}
+#endif
+{-# OPTIONS_GHC -W -Wall #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : AI.Search.Tree
+-- Copyright   : (c) 2014 Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2014-10-25
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- (Rose) tree search algorithms
+------------------------------------------------------------------------
+module AI.Search.Tree where
